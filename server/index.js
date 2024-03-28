4901a1004939ba727cb6d649a8481cb87f18b928
Celine Liu <mangolzj@gmail.com>
2024-03-28 11:11:52 -0400

    used express-fileupload to recieve data in the clothes endpoint

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
+
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
@@ -8,7 +10,9 @@ const clothesRoutes = require("./routes/clothes");
 const outfitsRoutes = require("./routes/outfits");
 const path = require("path");
 // const staticPath = path.join(__dirname, "/pictures");
+
 app.use(cors());
+app.use(fileUpload());
 // max size for the request body is 1mb
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
--- server/routes/clothes.js
+++ server/routes/clothes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { analyzer } = require("../controllers/analyzer");
+// const { analyzer } = require("../controllers/analyzer");
 // const getImages = require("../controllers/getImages.js");
 
 // Get a specific user by id
@@ -13,7 +13,30 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  res.send("image recieved");
+  errorMessages = [];
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+  if (!req.body.style) {
+    return res.status(400).send("No style is selected");
+  }
+
+  // Files are sent with dynamic keys like image0, image1, etc.
+  Object.keys(req.files).forEach((key) => {
+    const file = req.files[key];
+    // Process the file here (e.g., save to disk)
+    console.log(`Received file: ${file.name}`);
+
+    // Example: saving the file
+    // const uploadPath = __dirname + "/uploads/" + file.name;
+    // file.mv(uploadPath, function (err) {
+    //   if (err) {
+    //     return res.status(500).send(err);
+    //   }
+    // });
+  });
+
+  res.send("Files received!");
 });
 
 module.exports = router;
